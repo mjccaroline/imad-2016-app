d2b448789e6dbebe235bf3cd3f90174bb0186e9d
Maria Caroline Jose <mjcaroline@gmail.com>
2016-11-12 14:43:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,24 @@ app.use(morgan('combined'));
 var config= {
     database:'mjccaroline',
     user: 'mjccaroline',
-    port
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
 };
+
+var pool=new Pool(config);
+
+app.get('/test_db',function(req,res){
+   pool.query('SELECT * FROM test',function(){
+       if(err) {
+           res.status(500).send(err.toString());
+       }
+       else {
+           res.send(JSON.stringify(result));
+       }
+   }) 
+});
+
 var articles ={
  'article_one':{
    title:'Article-one',
