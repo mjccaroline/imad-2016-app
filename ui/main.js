ebb231faa0f0d125aa38ff12a3ed0bf96b4ffe13
mjccaroline <mjcaroline@gmail.com>
2016-11-12 13:02:05 +0530

    comments 1st step

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,16 @@ var img=document.getElementById('dp');
 var margintop=0;
 var timer=0;
 
-
+//implementing counter
 var button=document.getElementById('counter');
 button.onclick=function ()  {
   var request =new XMLHttpRequest();
 
   request.onreadystatechange=function() {
 	if(request.readyState===XMLHttpRequest.DONE) {
-
+    //if the get request is successfully loaded
 	if(request.status===200)  {
+    //extracting the response
 		var counter=request.responseText;
 		var span=document.getElementById('count');
 		span.innerHTML=counter.toString();
@@ -19,11 +20,12 @@ button.onclick=function ()  {
 	}
 
   };
+  //requesting a GEt http request
   request.open('GET','http://mjccaroline.imad.hasura-app.io/counter',true);
   request.send(null);
 };
 
-
+//for animation
 img.onclick=function () {
   timer=setInterval(entry,100);
 };
@@ -36,3 +38,29 @@ function entry() {
   }
   img.style.marginTop=margintop+'px';
 }
+
+//playing around with JSON; first step for adding comments
+var submitbtn=document.getElementById('sbt_btn');
+submitbtn.onclick() {
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function)() {
+
+    if(request.readyState===XMLHttpRequest.DONE){
+
+      if(request.status===200){
+
+        var names=request.responseText;
+        names=JSON.parse(names);
+        var list='';
+        for(var i=0;i<names.length;i++)  {
+          list+='<li>'+names[i]+'</li>';
+        }
+        var ulist=document.getElementById('namelist');
+        ulist.innerHTML=list;
+        }
+      }
+    };
+    var textinput=document.getElementById('name');
+    var name=textinput.value;
+    request.open('GET','http://mjccaroline.imad.hasura-app.io/submit_name?name='+name);
+  };
